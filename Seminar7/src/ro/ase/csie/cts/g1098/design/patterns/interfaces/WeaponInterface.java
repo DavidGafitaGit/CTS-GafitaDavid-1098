238c5afeb3edb6c70fda7a402da7d1648d5b9e73
david.gafita <gafitadavid19@stud.ase.ro>
2022-04-11 10:11:57 +0300

    Seminar7 - first commit

---
+++ Seminar7/src/ro/ase/csie/cts/g1098/design/patterns/interfaces/WeaponInterface.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.g1098.design.patterns.interfaces;
+
+public interface WeaponInterface {
+
+	public abstract void shoot();
+	public abstract void reload();
+}
---
+++ Seminar7/src/ro/ase/csie/cts/g1098/design/patterns/models/Superhero.java
@@ -0,0 +1,15 @@
+package ro.ase.csie.cts.g1098.design.patterns.models;
+
+import ro.ase.csie.cts.g1098.design.patterns.interfaces.WeaponInterface;
+
+public class Superhero {
+	
+	String name;
+	boolean isPositive;
+	int lifePoints;
+	boolean isCristialWounded;
+	
+	WeaponInterface leftHandWeapon;
+	WeaponInterface rightHandWeapon;
+
+}
